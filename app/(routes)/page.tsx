314b1c409de6642e93162c2e2c11b4c17151723b
クリス.コム <git@xn--pckua0m.xn--tckwe>
2025-04-01 23:27:23 +0000

    Nextjs boilerplate

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,13 @@
+// "use client";
+
+import Test from "#/components/test";
+
+export default function Home() {
+  return (
+    <div>
+      Hello, world!
+      {/* <button onClick={() => alert("Hello, world!")}>Click me</button> */}
+      <Test />
+    </div>
+  );
+}
